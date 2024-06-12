39d46677f0818cee214eb77c4e96d61c7dc95c3e
cyh000127 <okccc5@naver.com>
2024-06-12 17:31:40 +0900

    월별 사용내역 추가

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,30 +1,42 @@
 <template>
-  <tr>
-    <td>{{ todoItem.date }}</td>
-    <td>{{ todoItem.asset }}</td>
-    <td
-      :class="{
-        'text-blue': todoItem.transaction === '수입',
-        'text-red': todoItem.transaction === '지출' || todoItem.transaction === '이체',
-      }"
-    >
-      {{ todoItem.transaction }}
-    </td>
-    <td>{{ todoItem.type }}</td>
-    <td
-      :class="{
-        'text-blue': todoItem.transaction === '수입',
-        'text-red': todoItem.transaction === '지출' || todoItem.transaction === '이체',
-      }"
-    >
-      <!-- 수입이면 +를, 지출이나 이체이면 -를 앞에 붙입니다 -->
-      {{ (todoItem.transaction === '수입' ? '+' : '-') + Math.abs(todoItem.amount) }}
-    </td>
-    <td>{{ todoItem.memo }}</td>
-    <td>
-      <span class="float-end badge bg-secondary pointer m-1" @click="deleteTodo(todoItem.id)">삭제</span>
-    </td>
-  </tr>
+    <tr>
+        <td>{{ todoItem.date }}</td>
+        <td>{{ todoItem.asset }}</td>
+        <td
+            :class="{
+                'text-blue': todoItem.transaction === '수입',
+                'text-red':
+                    todoItem.transaction === '지출' ||
+                    todoItem.transaction === '이체',
+            }"
+        >
+            {{ todoItem.transaction }}
+        </td>
+        <td>{{ todoItem.type }}</td>
+        <td
+            :class="{
+                'text-blue': todoItem.transaction === '수입',
+                'text-red':
+                    todoItem.transaction === '지출' ||
+                    todoItem.transaction === '이체',
+            }"
+        >
+            <!-- 수입이면 +를, 지출이나 이체이면 -를 앞에 붙입니다 -->
+            {{
+                (todoItem.transaction === '수입' ? '+' : '-') +
+                Math.abs(todoItem.amount)
+            }}
+            원
+        </td>
+        <td>{{ todoItem.memo }}</td>
+        <td>
+            <span
+                class="float-end badge bg-secondary pointer m-1"
+                @click="deleteTodo(todoItem.id)"
+                >삭제</span
+            >
+        </td>
+    </tr>
 </template>
 <script setup>
 import { useRouter } from 'vue-router';
@@ -32,7 +44,7 @@ import { inject } from 'vue';
 import { defineProps } from 'vue';
 
 defineProps({
-  todoItem: { Type: Object, required: true },
+    todoItem: { Type: Object, required: true },
 });
 const router = useRouter();
 const { deleteTodo, toggleDone } = inject('actions');
@@ -41,10 +53,10 @@ const { deleteTodo, toggleDone } = inject('actions');
 <style scoped>
 /* 수입인 경우 파란색으로 설정합니다 */
 .text-blue {
-  color: blue;
+    color: blue;
 }
 /* 지출 또는 이체인 경우 빨간색 */
 .text-red {
-  color: red;
+    color: red;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 import TodoList from '@/pages/TodoList.vue';
 // import Home from '@/pages/Home.vue';
 import AddTodo from '@/pages/AddTodo.vue';
-import EditTodo from '@/pages/EditTodo.vue';
 import NotFound from '@/pages/NotFound.vue';
 import Calendar from '@/pages/Calendar.vue';
 const router = createRouter({
@@ -12,7 +11,6 @@ const router = createRouter({
         // { path: '/s', component: Home },
         { path: '/todos', component: TodoList },
         { path: '/todos/add', component: AddTodo },
-        { path: '/todos/edit/:id', component: EditTodo },
         { path: '/:paths(.*)*', component: NotFound },
     ],
 });
