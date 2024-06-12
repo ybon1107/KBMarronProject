766e348f2be3b6fc5af03636851096c0eefade53
Seo yejin <yejin1257@naver.com>
2024-06-12 09:44:05 +0900

    header 디자인 수정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="container">
-        <Header />
-        <!-- <router-view /> -->
-        <Loading v-if="states.isLoading" />
-    </div>
+  <div class="container">
+    <Header />
+    <!-- <router-view /> -->
+    <Loading v-if="states.isLoading" />
+  </div>
 </template>
 <script setup>
 import { reactive, computed, provide } from 'vue';
@@ -13,94 +13,94 @@ import Loading from '@/components/Loading.vue';
 const BASEURI = '/api/todos';
 const states = reactive({ todoList: [] });
 const fetchTodoList = async () => {
-    states.isLoading = true;
-    try {
-        const response = await axios.get(BASEURI);
-        if (response.status === 200) {
-            states.todoList = response.data;
-        } else {
-            alert('데이터 조회 실패');
-        }
-    } catch (error) {
-        alert('에러발생 :' + error);
+  states.isLoading = true;
+  try {
+    const response = await axios.get(BASEURI);
+    if (response.status === 200) {
+      states.todoList = response.data;
+    } else {
+      alert('데이터 조회 실패');
     }
-    states.isLoading = false;
+  } catch (error) {
+    alert('에러발생 :' + error);
+  }
+  states.isLoading = false;
 };
 const addTodo = async (
-    { type, transaction, asset, amount, date, memo },
-    successCallback
+  { type, transaction, asset, amount, date, memo },
+  successCallback
 ) => {
-    states.isLoading = true;
-    try {
-        const payload = { type, transaction, asset, amount, date, memo };
-        const response = await axios.post(BASEURI, payload);
-        if (response.status === 201) {
-            states.todoList.push({ ...response.data, done: false });
-            successCallback();
-        } else {
-            alert('Todo 추가 실패');
-        }
-    } catch (error) {
-        alert('에러발생 :' + error);
+  states.isLoading = true;
+  try {
+    const payload = { type, transaction, asset, amount, date, memo };
+    const response = await axios.post(BASEURI, payload);
+    if (response.status === 201) {
+      states.todoList.push({ ...response.data, done: false });
+      successCallback();
+    } else {
+      alert('Todo 추가 실패');
     }
-    states.isLoading = false;
+  } catch (error) {
+    alert('에러발생 :' + error);
+  }
+  states.isLoading = false;
 };
 const updateTodo = async (
-    { id, transaction, type, asset, amount, date, memo },
-    successCallback
+  { id, transaction, type, asset, amount, date, memo },
+  successCallback
 ) => {
-    states.isLoading = true;
-    try {
-        const payload = { id, transaction, type, asset, amount, date, memo };
-        const response = await axios.put(BASEURI + `/${id}`, payload);
-        if (response.status === 200) {
-            let index = states.todoList.findIndex((todo) => todo.id === id);
-            states.todoList[index] = payload;
-            successCallback();
-        } else {
-            alert('Todo 변경 실패');
-        }
-    } catch (error) {
-        alert('에러발생 :' + error);
+  states.isLoading = true;
+  try {
+    const payload = { id, transaction, type, asset, amount, date, memo };
+    const response = await axios.put(BASEURI + `/${id}`, payload);
+    if (response.status === 200) {
+      let index = states.todoList.findIndex((todo) => todo.id === id);
+      states.todoList[index] = payload;
+      successCallback();
+    } else {
+      alert('Todo 변경 실패');
     }
-    states.isLoading = false;
+  } catch (error) {
+    alert('에러발생 :' + error);
+  }
+  states.isLoading = false;
 };
 
 const deleteTodo = async (id) => {
-    states.isLoading = true;
-    try {
-        const response = await axios.delete(BASEURI + `/${id}`);
-        console.log(response.status, response.data);
-        if (response.status === 200) {
-            let index = states.todoList.findIndex((todo) => todo.id === id);
-            states.todoList.splice(index, 1);
-        } else {
-            alert('Todo 삭제 실패');
-        }
-    } catch (error) {
-        alert('에러발생 :' + error);
+  states.isLoading = true;
+  try {
+    const response = await axios.delete(BASEURI + `/${id}`);
+    console.log(response.status, response.data);
+    if (response.status === 200) {
+      let index = states.todoList.findIndex((todo) => todo.id === id);
+      states.todoList.splice(index, 1);
+    } else {
+      alert('Todo 삭제 실패');
     }
-    states.isLoading = false;
+  } catch (error) {
+    alert('에러발생 :' + error);
+  }
+  states.isLoading = false;
 };
 const toggleDone = async (id) => {
-    states.isLoading = true;
-    try {
-        let todo = states.todoList.find((todo) => todo.id === id);
-        let payload = { ...todo, done: !todo.done };
-        const response = await axios.put(BASEURI + `/${id}`, payload);
-        if (response.status === 200) {
-            todo.done = payload.done;
-        } else {
-            alert('Todo 완료 변경 실패');
-        }
-    } catch (error) {
-        alert('에러발생 :' + error);
+  states.isLoading = true;
+  try {
+    let todo = states.todoList.find((todo) => todo.id === id);
+    let payload = { ...todo, done: !todo.done };
+    const response = await axios.put(BASEURI + `/${id}`, payload);
+    if (response.status === 200) {
+      todo.done = payload.done;
+    } else {
+      alert('Todo 완료 변경 실패');
     }
-    states.isLoading = false;
+  } catch (error) {
+    alert('에러발생 :' + error);
+  }
+  states.isLoading = false;
 };
 provide(
-    'todoList',
-    computed(() => states.todoList)
+  'todoList',
+  computed(() => states.todoList)
 );
 provide('actions', { addTodo, deleteTodo, updateTodo, fetchTodoList });
 fetchTodoList();
--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="row">
-    <div class="col p-3">
-      <router-link class="btn btn-primary" to="/todos/add">
-        할일 추가
-      </router-link>
+    <div class="col">
+      <!-- 테이블 및 기타 컨텐츠 -->
+      <table class="table">
+        <thead>
+          <tr>
+            <th>날짜</th>
+            <th>거래 유형</th>
+            <th>자산</th>
+            <th>분류</th>
+            <th>금액</th>
+            <th>내용</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TodoItem
+            v-for="todoItem in todoList"
+            :key="todoItem.id"
+            :todoItem="todoItem"
+          />
+        </tbody>
+      </table>
     </div>
   </div>
-  <div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>날짜</th>
-          <th>거래 유형</th>
-          <th>자산</th>
-          <th>분류</th>
-          <th>금액</th>
-          <th>내용</th>
-        </tr>
-      </thead>
-      <tbody>
-        <TodoItem
-          v-for="todoItem in todoList"
-          :key="todoItem.id"
-          :todoItem="todoItem"
-        />
-      </tbody>
-    </table>
+  <!-- "할일 추가" 버튼을 오른쪽 하단에 고정 -->
+  <div class="custom-btn-container">
+    <router-link class="custom-btn" to="/todos/add">
+      <i class="fas fa-plus"></i>
+    </router-link>
   </div>
 </template>
 
@@ -45,5 +47,26 @@ const deleteTodoItem = (id) => {
 </script>
 
 <style scoped>
-/* 필요한 경우 스타일링을 추가하세요 */
+.custom-btn-container {
+  position: fixed;
+  bottom: 140px;
+  right: 100px;
+}
+
+.custom-btn {
+  background-color: #ff6347; /* 원하는 버튼 색상 */
+  color: white; /* 텍스트 색상 */
+  width: 60px; /* 버튼의 너비 */
+  height: 60px; /* 버튼의 높이 */
+  border-radius: 50%; /* 버튼을 원형으로 만듦 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px; /* 텍스트 크기 */
+  text-decoration: none;
+}
+
+.custom-btn:hover {
+  background-color: #ff4500; /* 호버 시 버튼 색상 */
+}
 </style>
